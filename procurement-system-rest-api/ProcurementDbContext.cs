ecc88cbb3372656ea617c286d35022b0b14cc1b8
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-30 12:35:44 +0530

    init procurement db context

---
+++ procurement-system-rest-api/ProcurementDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace procurement_system_rest_api
+{
+    public class ProcurementDbContext: DbContext
+    {
+        public ProcurementDbContext(DbContextOptions<ProcurementDbContext> options) : base(options) {}
+    }
+}
